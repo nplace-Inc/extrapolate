104a38f46317f335654fd2efe0dd883e582ab79f
Nplace-su <105898543+Nplace-su@users.noreply.github.com>
2023-04-04 19:12:37 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,7 +139,7 @@ export default function Home() {
           )}
 	{generatedImage && (
 	  <img
-	    className="block mx-auto w-full lg:w-4/5 xl:w-3/4 mx-auto my-8 shadow-lg rounded-lg transform hover:scale-105 hover:rotate-3 transition-all duration-500 ease-in-out border-4 border-indigo-500 hover:border-green-500 object-cover mt-6 max-w-full z-9999"
+	    className="block mx-auto w-full lg:w-4/5 xl:w-3/4 mx-auto my-8 shadow-lg rounded-lg transform hover:scale-105 transition-all duration-500 ease-in-out border-4 border-pink-500 hover:border-purple-600 border-opacity-75 border-dashed object-cover mt-6 max-w-full z-9999"
 	    src={generatedImage}
 	    alt="Generated Image"
 	  />
