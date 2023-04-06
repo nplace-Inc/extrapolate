e330e0aae5e1cc8771ae0c6795b994141492d236
Nplace-su <105898543+Nplace-su@users.noreply.github.com>
2023-04-06 13:23:07 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -139,7 +139,7 @@ export default function Home() {
           )}
 	{generatedImage && (
 	  <img
-	    className="block mx-auto h-auto max-w-lg transition-all duration-300 rounded-lg blur-sm hover:blur-none mt-6 max-w-full z-9999"
+	    className="block mx-auto h-auto max-w-lg hadow-lg rounded max-w-full h-auto align-middle border-none mt-6 max-w-full z-9999"
 	    src={generatedImage}
 	    alt="Generated Image"
 	  />
